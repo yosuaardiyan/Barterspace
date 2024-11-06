3cd5e07e9fd4ed3d56d0e2eba93b708668cec5fc
yosuaardiyan <yosiferiardiyan96@gmail.com>
2024-11-06 11:31:08 +0700

    lol

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('image-animation');
+      }
+    });
+  });
+  
+  const viewbox = document.querySelectorAll('.image');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  });
